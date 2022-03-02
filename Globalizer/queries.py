f76fd51b12d6ee8df1cae432ef788edb18a7005c
Arvin Azimifard <arvinazimifard@gmail.com>
2022-03-02 12:00:49 +0100

    I created a new file, where we do the queries, to have a lean predict.py

---
+++ Globalizer/queries.py
@@ -0,0 +1,26 @@
+import sqlite3
+import pandas as pd
+
+# path to local database
+PATH_TO_DB = 'raw_data/countrypop(2).db'
+
+# connect to database
+# path to local database
+PATH_TO_DB = '../raw_data/countrypop(2).db'
+
+# connect to database
+def get_data(country):
+    '''This function get's the Countrydata from a local Database
+    and converts it to a Dataframe'''
+    conn = sqlite3.connect(PATH_TO_DB)
+
+    param = (country)
+    query = (f"""
+                SELECT lon, lat, pop
+                FROM {param}
+
+                """)
+
+    data = pd.read_sql_query(query.format(param), con=conn)
+
+    return data
