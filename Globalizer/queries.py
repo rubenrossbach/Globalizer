603f1815aef00125f4c6f605e16ee472138159f8
Arvin Azimifard <arvinazimifard@gmail.com>
2022-03-03 11:48:19 +0100

    Adjusted the query and prediction to the 3d_df

--- Globalizer/queries.py
+++ Globalizer/queries.py
@@ -2,7 +2,7 @@ import sqlite3
 import pandas as pd
 
 # path to local database
-PATH_TO_DB = 'raw_data/countrypop(2).db'
+PATH_TO_DB = 'raw_data/countrypop_small19.db'
 
 # not sure if we still need this function
 def get_data(country):
@@ -13,27 +13,24 @@ def get_data(country):
 
     param = (country)
     query = ("""
-                SELECT lon, lat, pop
+                SELECT x, y, z, pop
                 FROM {}
-
                 """)
 
     data = pd.read_sql_query(query.format(param), con=conn)
-
     return data
 
 
 def advanced_get_data(country_lst):
-    '''This function get's a list of countries and merges them into 1 dataframeS'''
-    print(country_lst)
+    '''This function get's a list of countries and merges them into 1 dataframes'''
     conn = sqlite3.connect(PATH_TO_DB)
     frames = []
     #iterating over the list, and create a new list, with dfs as elements
     for country in country_lst:
         param = (country)
-        query = (f"""
-                    SELECT lon, lat, pop
-                    FROM {param}
+        query = ("""
+                    SELECT x, y, z, pop
+                    FROM {}
 
                     """)
 
