e2a6ef981acbadb5e58e29266131801bed700119
Martin Reichardt <mrlogin@mailbox.org>
2022-03-09 11:33:04 +0100

    Changed the k_clustering method for minibatch kmeans and implemented the radius function in the predict api

--- api/fast.py
+++ api/fast.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from Globalizer.predict import k_clustering, smart_clustering
+from Globalizer.predict import k_clustering, radius_clustering, smart_clustering
 app = FastAPI()
 
 app.add_middleware(
@@ -17,19 +17,26 @@ def index():
     return {"ok": True}
 
 @app.get("/predict")
-def predict(country, threshold=50, n_centers=None):
+def predict(country, threshold=None, n_centers=None, radius=None, percent=None):
     '''This function controls whether
     we use the Kmeans or the KmeansMinibatch. By default it's
     the Minibatch with 50km as threshold'''
     country = country.split(",")
-    threshold= float(threshold)
     if n_centers != None:
         n_centers= int(n_centers)
-    return smart_clustering(country, threshold) if n_centers == None else k_clustering(country, n_centers)
+        return k_clustering(country, n_centers)
+    if threshold != None:
+        threshold= float(threshold)
+        return smart_clustering(country, threshold)
+    if radius != None and percent != None:
+        radius = int(radius)
+        percent = int(percent)
+        return radius_clustering(country, radius, percent)
 
 
 ## Check ##
 # to check your code, run:
 # python -m api.fast
 if __name__=="__main__":
-    print(predict(country = "ABW", n_centers = None))
+    #print(predict(country = "ABW", n_centers = 5))
+    print(predict(country = "AUT", radius=60, percent=80))
